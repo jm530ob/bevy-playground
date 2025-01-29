63ead4b49293decb3cb3647d3d586ece2b75ec84
Jakub Martenek <j.martenek@azet.sk>
2025-01-29 15:17:48 +0100

    testing assets

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,9 @@ impl Movable {
     }
 }
 
+#[derive(Component)]
+struct Ground;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -33,6 +36,7 @@ fn main() {
         .add_systems(Startup, setup)
         .add_systems(Update, robot_animation)
         .add_systems(Update, move_model)
+        .add_systems(Update, draw_cursor)
         .run();
 }
 
@@ -84,12 +88,49 @@ pub fn setup(
     commands.spawn((
         Mesh3d(meshes.add(Plane3d::default().mesh().size(20., 20.))),
         MeshMaterial3d(materials.add(Color::from(DARK_GREEN))),
+        Ground,
     ));
 
     commands.spawn((
         Transform::from_translation(Vec3::new(0.0, 1.5, 5.0)),
         PanOrbitCamera::default(),
     ));
+}
+
+fn draw_cursor(
+    camera_query: Single<(&Camera, &GlobalTransform)>,
+    ground: Single<&GlobalTransform, With<Ground>>,
+    windows: Single<&Window>,
+    mut gizmos: Gizmos,
+) {
+    let (camera, camera_transform) = *camera_query;
+
+    let Some(cursor_position) = windows.cursor_position() else {
+        return;
+    };
+
+    // Calculate a ray pointing from the camera into the world based on the cursor's position.
+    let Ok(ray) = camera.viewport_to_world(camera_transform, cursor_position) else {
+        return;
+    };
+
+    // Calculate if and where the ray is hitting the ground plane.
+    let Some(distance) =
+        ray.intersect_plane(ground.translation(), InfinitePlane3d::new(ground.up()))
+    else {
+        return;
+    };
+    let point = ray.get_point(distance);
+
+    // Draw a circle just above the ground plane at that position.
+    gizmos.circle(
+        Isometry3d::new(
+            point + ground.up() * 0.01,
+            Quat::from_rotation_arc(Vec3::Z, ground.up().as_vec3()),
+        ),
+        0.2,
+        Color::WHITE,
+    );
 }
 
 pub fn robot_animation(
